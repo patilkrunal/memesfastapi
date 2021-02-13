243322f6d1565eb0524dff498473fff317637bd3
gitkp <47073116+gitkp11@users.noreply.github.com>
2021-02-14 01:13:12 +0530

    Update app.py

--- app/server/app.py
+++ app/server/app.py
@@ -10,7 +10,7 @@ origins = [
     # "http://localhost:8000",
     # "http://localhost:5500",
     "https://memefastapi.herokuapp.com/meme/",
-    "http://127.0.0.1:5500",
+    "https://gitkp11.github.io/memesfastapi/",
 ]
 
 app.add_middleware(
