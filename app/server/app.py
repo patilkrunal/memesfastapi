f0a5f42c3828b2ce063703ceaff41c1b32687e6f
Krunal Patil <47073116+patilkrunal@users.noreply.github.com>
2021-02-26 12:31:07 +0530

    Update app.py

--- app/server/app.py
+++ app/server/app.py
@@ -5,7 +5,7 @@ from .routes.meme import router as MemeRouter
 app = FastAPI()
 origins = [
     "https://diwakarsingh18277.github.io",
-    "https://patilkrunal.github.io/memesfastapi/",
+    "https://patilkrunal.github.io",
     "http://127.0.0.1:5500"
 ]
 
